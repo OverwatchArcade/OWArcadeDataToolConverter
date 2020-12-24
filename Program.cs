85e98d1df59a2452cfced1f1b28d1dc366d2ba45
Quinten Peels <quinten@peels.me>
2020-12-24 11:11:09 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OWArcadeBackend.Models;
+using OWArcadeBackend.Models.Overwatch;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OWArcadeDataToolConverter
+{
+    class Program : Util
+    {
+        public static string rootDirectory = "C:\\Users\\quint\\source\\repos\\OWArcadeDataToolConverter";
+        public static string inDirectory = rootDirectory+"\\in\\";
+        public static string OutDirectory = rootDirectory+"\\out\\";
+        public static string imageInDirectory = inDirectory+"\\images\\";
+        public static string imageOutDirectory = OutDirectory+"\\images\\";
+        static void Main(string[] args)
+        {
+            ClearImageOutputDirectory(imageOutDirectory);
+
+            List<ArcadeMode> modes = JsonConvert.DeserializeObject<List<ArcadeMode>>(File.ReadAllText(inDirectory + "modes.json"), new ArcadeModeJsonConverter());
+            modes = modes.Where(x => x.Players != null).ToList();
+            using (var writer = new StreamWriter(OutDirectory + "modes.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.RegisterClassMap<ArcadeModeMap>();
+                csv.WriteRecords(modes);
+            }
+
+            Console.WriteLine();
+        }
+
+        public class ArcadeModeJsonConverter : JsonConverter<ArcadeMode>
+        {
+            public override ArcadeMode ReadJson(JsonReader reader, Type objectType, [AllowNull] ArcadeMode existingValue, bool hasExistingValue, JsonSerializer serializer)
+            {
+                JObject obj = JObject.Load(reader);
+                ArcadeMode mode = new ArcadeMode();
+
+                
+                mode.Name = (string)obj["Name"];
+                mode.Description = ((string)obj["Description"]);
+                mode.Game = 0;
+                mode.Image = CalculateImageFilename(obj);
+                // Sometimes modes contain weird characters. We have to rename mode name and the associated file.
+                if (mode.Name.Contains("tx0C0000000001FEE9"))
+                {
+                    string[] cleanFileNames = RemoveShitFromName(obj);
+                    // Copy filee with correct naming to output directory
+                    CopyFileAs(imageInDirectory, imageInDirectory, mode.Image, cleanFileNames[0]);
+                    mode.Image = cleanFileNames[0];
+                    mode.Name = cleanFileNames[1];
+                }
+                if (obj["About"].Count() > 0)
+                {
+                    mode.Players = (string)obj["About"][0];
+                }
+
+                // Rename image file to md5 file (modename + modeplayers)
+                string md5ImageName = CreateMD5(mode.Name + mode.Players) + ".jpg";
+                CopyFileAs(imageInDirectory, imageOutDirectory, mode.Image, md5ImageName);
+                mode.Image = md5ImageName;
+                return mode;
+            }
+
+            public override void WriteJson(JsonWriter writer, [AllowNull] ArcadeMode value, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public sealed class ArcadeModeMap : ClassMap<ArcadeMode>
+        {
+            public ArcadeModeMap()
+            {
+                AutoMap(CultureInfo.InvariantCulture);
+                Map(m => m.Id).Ignore();
+            }
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text;
+
+namespace OWArcadeDataToolConverter
+{
+    public class Util
+    {
+
+        public static void ClearImageOutputDirectory(string ImageOutputDirectory)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(ImageOutputDirectory);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            Console.WriteLine("--- Cleared image directory ---");
+        }
+
+        public static void CopyFileAs(string imageDirectory, string ImageOutputDirectory, string currentFileName, string newFileName)
+        {
+            try
+            {
+                File.Copy(imageDirectory + "/" + currentFileName, ImageOutputDirectory + "/" + newFileName);
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.WriteLine("File not found: {0}", currentFileName);
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine("Something went wrong with file" + currentFileName);
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                return BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(input)))
+                    .Replace("-", string.Empty);
+            }
+        }
+
+        public static string CalculateImageFilename(JObject modeObj)
+        {
+            // First calculate filename (i.e Total Mayhem_D.JPG, Mirrored Deathmatch _tx0C0000000001FEE9__A8.JPG)
+            string GUID = ((string)modeObj["GUID"]).TrimStart(new char[] { '0' }); // Remove leading zeros
+            GUID = GUID.Substring(0, GUID.LastIndexOf('.')); // Select everything leading till 
+
+            return ((string)modeObj["Name"]).Replace(":", "_").Replace("<", "_").Replace(">", "_").TrimEnd() + "_" + GUID + ".JPG";
+        }
+
+        public static string[] RemoveShitFromName(JObject modeObj)
+        {
+            var test = CalculateImageFilename(modeObj);
+            string newimageFileName = test.Replace(" _tx0C0000000001FEE9_", "");
+            string newModeName = ((string)modeObj["Name"]).Replace(" <tx0C0000000001FEE9>", "");
+
+            return new string[] { newimageFileName, newModeName };
+        }
+    }
+}
